42030ad3d9015bc2e312ef4a6c5edafc08c4ad20
Priyansh Dimri <priyanshd0908@gmail.com>
2025-01-01 19:29:19 +0530

    add gameBoard factory function of type IIFE

--- script.js
+++ script.js
@@ -8,3 +8,44 @@ function createPlayer(name) {
 
   return { name, changeName, increaseScore, resetScore };
 }
+
+const gameBoard = (function () {
+  let gameBoardList = new Array(9);
+
+  const winningPositions = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  const playMove = (symbol, idx) => {
+    if (!gameBoardList[idx]) {
+      gameBoardList[idx] = symbol;
+    }
+  };
+
+  const clearBoard = () => gameBoardList.fill(undefined);
+
+  const checkPlayerWon = () => {
+    for (let positions in winningPositions) {
+      const [idx1, idx2, idx3] = positions;
+
+      if (
+        ((gameBoardList[idx1] === gameBoardList[idx2]) ===
+          gameBoardList[idx3]) !==
+        undefined
+      ) {
+        return gameBoardList[idx1];
+      }
+    }
+
+    return "";
+  };
+
+  return { playMove, clearBoard, checkPlayerWon };
+})();
